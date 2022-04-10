76c4a5df7a93233c6a19b91b263af7af3e94a6c4
FlameHashira <103308579+FlameHashira@users.noreply.github.com>
2022-04-10 22:40:56 +0530

    Create Access value of List from Dictionary.py

---
+++ Access value of List from Dictionary.py
@@ -0,0 +1,23 @@
+   #  Creating dictionary which contains lists 
+
+country = { 
+
+    "India": ["Delhi", "Maharastra", "Haryana",  
+
+              "Uttar Pradesh", "Himachal Pradesh"], 
+
+    "Japan": ["Hokkaido", "Chubu", "Tohoku", "Shikoku"], 
+
+    "United States": ["New York", "Texas", "Indiana",  
+
+                      "New Jersey", "Hawaii", "Alaska"] 
+} 
+
+  
+
+
+print(country["India"][1]) 
+
+print(country["United States"][3]) 
+
+print(country['Japan'][2])
